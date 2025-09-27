791323ae61160dc90d4a8457c5eb8a11dccbab59
jeannettearias <jeannettearias@icloud.com>
2025-09-27 14:43:43 +0200

    starting the hero page

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -3,8 +3,21 @@ import "../../styles/_hero.scss";
 function Hero() {
     return (
         <>
-            <section>
-                <h1>Soy Antonia, product Designer</h1>
+            <section className="hero">
+                <div className="hero__content">
+                    <div className="Tag">
+                        <label className="tag__label">Hello there</label>
+                    </div>
+                    <div className="hero__content__content">
+                        <h1 className="hero__title">Soy Antonia, product Designer</h1>
+                        <p className="hero__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                    <div className="group-buttons">
+                        <button>Contacto</button>
+                        <button>Descargar CV</button>
+
+                    </div>
+                </div>
 
             </section>
         </>
