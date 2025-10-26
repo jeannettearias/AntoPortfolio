6f21553b4d99609fb64b9c9ccda35cb14f0bfd7a
jeannettearias <jeannettearias@icloud.com>
2025-10-26 22:37:48 +0100

    last version of aboutMe

--- src/components/landing/Bar.jsx
+++ src/components/landing/Bar.jsx
@@ -6,11 +6,21 @@ function Bar() {
             <div className="bar">
                 <div className="bar-info">
                     <ul className='group-cards'>
-                        <li className='label'>Product Designer</li>
-                        <li className='label'>UI/UX Designer</li>
-                        <li className='label'>Visual Designer</li>
-                        <li className='label'>Design System</li>
-                        <li className='label'>UX Researcher</li>
+                        <li className='label'>Product Designer
+                            <img src="/images/icons/Bullets.svg" alt="" />
+                        </li>
+                        <li className='label'>UI/UX Designer
+                            <img className='icon-bar' alt="" />
+                        </li>
+                        <li className='label'>Visual Designer
+                            <img className='icon-bar' alt="" />
+                        </li>
+                        <li className='label'>Design System
+                            <img className='icon-bar' alt="" />
+                        </li>
+                        <li className='label'>UX Researcher
+                            <img className='icon-bar' alt="" />
+                        </li>
                     </ul>
                 </div>
             </div>
