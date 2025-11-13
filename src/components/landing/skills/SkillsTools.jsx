88d81ea4b782db9d2cfa4e742bf57ab8f4bfa8af
jeannettearias <jeannettearias@icloud.com>
2025-11-13 21:02:33 +0100

    include figma logos

--- src/components/landing/skills/SkillsTools.jsx
+++ src/components/landing/skills/SkillsTools.jsx
@@ -3,10 +3,46 @@ function SkillsTools() {
         <>
             <section className='Section-tools'>
                 <div className="text-tools">
-
+                    <div className="Badge-text">
+                        <div className="Tag-badge"></div>
+                        <label className="label-badge">Sobre mi</label>
+                    </div>
+                    <div className="Content-text">
+                        <title className="title-text">Las herramientas detrás de mis diseños</title>
+                    </div>
+                    <div className="Group-tag"></div>
                 </div>
                 <div className="Group-info-tools">
-
+                    <ul className="item-group-info">
+                        <div className="item-label">
+                            <img className="img-item" src="images/icons/figma-Icon.svg" alt="Figma" />
+                            <label htmlFor="figma-icon">Figma</label>
+                        </div>
+                        <div className="item-label">
+                            <img className="img-item" src="images/icons/figma-Icon.svg" alt="After Effects" />
+                            <label htmlFor="after-effects-icon">After Effects</label>
+                        </div>
+                        <div className="item-label">
+                            <img className="img-item" src="images/icons/figma-Icon.svg" alt="Visual Studio Code" />
+                            <label htmlFor="visual-studio-code-icon">Visual Studio Code</label>
+                        </div>
+                        <div className="item-label">
+                            <img className="img-item" src="images/icons/figma-Icon.svg" alt="Adobe Photoshop" />
+                            <label htmlFor="adobe-photoshop-icon">Adobe Photoshop</label>
+                        </div>
+                        <div className="item-label">
+                            <img className="img-item" src="images/icons/figma-Icon.svg" alt="Adobe Illustrator" />
+                            <label htmlFor="adobe-illustrator-icon">Adobe Illustrator</label>
+                        </div>
+                        <div className="item-label">
+                            <img className="img-item" src="images/icons/figma-Icon.svg" alt="3DS Max" />
+                            <label htmlFor="3ds-max-icon">3DS Max</label>
+                        </div>
+                        <div className="item-label">
+                            <img className="img-item" src="images/icons/figma-Icon.svg" alt="Spline" />
+                            <label htmlFor="spline-icon">Spline</label>
+                        </div>
+                    </ul>
                 </div>
             </section>
         </>
