f4e32b6b3c64940b0bd33d8d2e514f379ce27f6b
jeannettearias <jeannettearias@icloud.com>
2025-11-13 23:00:40 +0100

    project component first version

--- src/components/landing/Group-sections/GroupContactUs.jsx
+++ src/components/landing/Group-sections/GroupContactUs.jsx
@@ -1,12 +1,19 @@
-import '../../../styles/_groupSections.scss';
-
+import '../../../styles/_barGroupContactUs.scss';
+import GroupProjects from './GroupProjects';
 
 function GroupContactUs() {
     return (
         <>
+            <section className='Bar_contactUs'>
+                <div className='Detail_contactUs'></div>
+            </section>
+
+            <GroupProjects />
+
             <section id="ContactMe" className="contact-me-section">
 
             </section>
+
 
         </>
     );
