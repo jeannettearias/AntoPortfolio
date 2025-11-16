d80175a8329315aa2abfc9de706f86e9d2760b68
jeannettearias <jeannettearias@icloud.com>
2025-11-16 23:08:48 +0100

    group experience sections first style

--- src/components/landing/Group-sections/GroupExperience.jsx
+++ src/components/landing/Group-sections/GroupExperience.jsx
@@ -1,22 +1,22 @@
-import '../../../styles/_groupSections.scss';
+import '../../../styles/_groupExperience.scss';
 
 function GroupExperience() {
     return (
         <section id="GroupExperience" className="experience-section">
             <div className='Content-exp'>
-                <div className="Badge">
-                    <label className='Badge-text'>Sobre mí</label>
+                <div className='Badge'>
+                    <img className="badge__tag" src="" alt="" />
+                    <label className='badge__label' >Sobre mí</label>
                 </div>
-
                 <p className="content-text">Mis <span className="green-text">Estudios y Experiencia</span></p>
-
             </div>
             <div className='group-exp-card'>
                 <div className='card__1'>
                     <div className='head-exp'>
-                        <img className="icon__exp" src="" alt="" />
-                        <label className='label__exp'></label>
+                        <img className="icon__exp" alt="" />
+                        <label className='label__exp'>Educación</label>
                     </div>
+                    <div className='divider'></div>
                 </div>
                 <div className='card__2'>
                     <div className='head-exp'></div>
