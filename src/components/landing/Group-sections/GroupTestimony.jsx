611f3de24a7c6a5db54ce334351cfc4cd368f57d
Jeannette Arias <81124142+jeannettearias@users.noreply.github.com>
2026-02-04 18:33:29 +0100

    Revert "starting testimony component"

--- src/components/landing/Group-sections/GroupTestimony.jsx
+++ src/components/landing/Group-sections/GroupTestimony.jsx
@@ -1,5 +1,4 @@
-import '../../../styles/_testimony.scss';
-
+import '../../../styles/_groupSections.scss';
 
 
 function GroupTestimony() {
@@ -7,22 +6,6 @@ function GroupTestimony() {
         <>
             <section id="References" className="references-section">
 
-                <div className='image_testimony'>
-                    <img src="images/testimony/world_picture.png" alt="" />
-                </div>
-                <div className='text_testimony'>
-                    <div className='Badge'>
-                        <label className='label_badge'>Referencias</label>
-                    </div>
-                    <div className='content_testimony'>
-                        <label className='title_testimony_content'>El impacto de mi trabajo</label>
-                    </div>
-
-                </div>
-                <div className='group_testimony_cards'>
-                    <div className='margin_card'></div>
-
-                </div>
             </section>
         </>
     );
