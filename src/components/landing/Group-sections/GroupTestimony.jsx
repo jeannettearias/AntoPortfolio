7b524e04d74cd0c3d9249e3d2ab9c257ae355e4c
jeannettearias <jeannettearias@icloud.com>
2026-02-04 18:40:45 +0100

    grouptestimony component

--- src/components/landing/Group-sections/GroupTestimony.jsx
+++ src/components/landing/Group-sections/GroupTestimony.jsx
@@ -1,4 +1,5 @@
-import '../../../styles/_groupSections.scss';
+import '../../../styles/_testimony.scss';
+
 
 
 function GroupTestimony() {
@@ -6,6 +7,22 @@ function GroupTestimony() {
         <>
             <section id="References" className="references-section">
 
+                <div className='image_testimony'>
+                    <img src="images/testimony/world_picture.png" alt="" />
+                </div>
+                <div className='text_testimony'>
+                    <div className='Badge'>
+                        <label className='label_badge'>Referencias</label>
+                    </div>
+                    <div className='content_testimony'>
+                        <label className='title_testimony_content'>El impacto de mi trabajo</label>
+                    </div>
+
+                </div>
+                <div className='group_testimony_cards'>
+                    <div className='margin_card'></div>
+
+                </div>
             </section>
         </>
     );
